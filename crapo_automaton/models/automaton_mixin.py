af4db974006e7f90fce4a93f19294952da4c19a9
Simon COLLIN <simon.collin@tekfor.cloud>
2024-01-18 19:16:33 +0100

    [14.0] Fix tracking

--- crapo_automaton/models/automaton_mixin.py
+++ crapo_automaton/models/automaton_mixin.py
@@ -52,7 +52,7 @@ class CrapoAutomatonMixin(models.AbstractModel):
         ],
         group_expand="_read_group_crapo_states",
         copy=False,
-        track_visibility=True,
+        tracking=True,
     )
 
     crapo_readonly_fields = fields.Char(
