cf44fa173c376c2885d80f5ee96fa8b414e48009
slugand <stephane.lugand@tekfor.cloud>
2025-04-28 15:28:15 +0200

    ajout logging

--- crapo_workflow/models/base.py
+++ crapo_workflow/models/base.py
@@ -2,6 +2,7 @@
 see README for details
 """
 
+import logging
 from odoo import models
 
 
@@ -35,7 +36,9 @@ class Base(models.AbstractModel):
             if values is None:
                 values = {}
             values["record"] = rec
+            logging.info("BEFORE is_superuser:%s", rec.env.is_superuser())
             broker = self.copy_context(rec, broker)
+            logging.info("AFTER is_superuser:%s", broker.env.is_superuser())
             broker.with_delay().notify(name, values)
 
     def copy_context(self, from_model, to_model):
